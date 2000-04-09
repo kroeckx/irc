84b6be49fe7c8d9660cf2386724e27990dda8935
Kurt Roeckx <kurt@roeckx.be>
2000-04-09 19:06:14 +0000

    We need to set the user first before adding him to the hash table

--- ircd/s_user.c
+++ ircd/s_user.c
@@ -22,7 +22,7 @@
  */
 
 #ifndef lint
-static  char rcsid[] = "@(#)$Id: s_user.c,v 1.86.2.4 2000/04/09 15:06:32 q Exp $";
+static  char rcsid[] = "@(#)$Id: s_user.c,v 1.86.2.5 2000/04/09 19:06:14 q Exp $";
 #endif
 
 #include "os.h"
@@ -964,6 +964,7 @@ nickkilldone:
 	if (IsServer(sptr))
 	    {
 		char	*pv[7];
+		int	i;
 
 		if (parc != 8)
 		    {
@@ -989,8 +990,12 @@ nickkilldone:
 		pv[4] = parv[7];
 		pv[5] = parv[6];
 		pv[6] = NULL;
-		(void)add_to_client_hash_table(nick, sptr);
-		return m_user(cptr, sptr, 6, pv);
+		i = m_user(cptr, sptr, 6, pv);
+		if (i > 0)
+		    {
+			(void)add_to_client_hash_table(nick, sptr);
+		    }
+		return i;
 	    }
 	else if (sptr->name[0])		/* NICK received before, changing */
 	    {
