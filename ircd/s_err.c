7733ddd1dd33ba0938b259625dd0536192195651
Kurt Roeckx <kurt@roeckx.be>
2001-02-08 01:41:05 +0000

    Numeric 005 was used for alot different things.  It wasn't easy to parse for clients either.  It's now moved to numeric 010, just line hybrid does it.

--- ircd/s_err.c
+++ ircd/s_err.c
@@ -18,7 +18,7 @@
  */
 
 #ifndef lint
-static  char rcsid[] = "@(#)$Id: s_err.c,v 1.24.2.3 2001/02/08 01:37:35 q Exp $";
+static  char rcsid[] = "@(#)$Id: s_err.c,v 1.24.2.4 2001/02/08 01:41:06 q Exp $";
 #endif
 
 #include "os.h"
@@ -41,7 +41,12 @@ static	Numeric	local_replies[] = {
 /* 002 */	{ RPL_YOURHOST, ":Your host is %s, running version %s" },
 /* 003 */	{ RPL_CREATED, ":This server was created %s" },
 /* 004 */	{ RPL_MYINFO, "%s %s aoOirw abeiIklmnoOpqrstv" },
-/* 005 */	{ RPL_BOUNCE, ":Try server %s, port %d" },
+		{ 0, (char *)NULL },
+		{ 0, (char *)NULL },
+		{ 0, (char *)NULL },
+		{ 0, (char *)NULL },
+		{ 0, (char *)NULL },
+/* 010 */	{ RPL_BOUNCE, "%s %d :Please use this other server" },
 		{ 0, (char *)NULL }
 };
 
