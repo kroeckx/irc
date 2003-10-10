ff8f30ac9536de87629a06694ebd4d95527ab1cb
Piotr Kucharski <chopin@42.pl>
2003-10-10 22:52:28 +0000

    stats l remoteclient bug (backport)

--- ircd/s_serv.c
+++ ircd/s_serv.c
@@ -22,7 +22,7 @@
  */
 
 #ifndef lint
-static  char rcsid[] = "@(#)$Id: s_serv.c,v 1.65.2.7 2001/10/19 21:09:10 chopin Exp $";
+static  char rcsid[] = "@(#)$Id: s_serv.c,v 1.65.2.8 2003/10/10 22:52:28 chopin Exp $";
 #endif
 
 #include "os.h"
@@ -1428,7 +1428,7 @@ char	*parv[];
 		    }
 		else
 		    {
-			if ((acptr = find_client(cm, NULL)))
+			if ((acptr = find_client(cm, NULL)) && MyConnect(acptr))
 				sendto_one(cptr, Lformat, ME,
 					RPL_STATSLINKINFO, parv[0],
 					get_client_name(acptr, isupper(stat)),
