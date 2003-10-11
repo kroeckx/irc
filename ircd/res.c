80fcda8d6a74fc6e1dce9b95259af7961a314adc
Piotr Kucharski <chopin@42.pl>
2003-10-11 13:25:14 +0000

    ip6.int and ip6.arpa, also SIXBONE_HACK defined default

--- ircd/res.c
+++ ircd/res.c
@@ -24,7 +24,7 @@
 #undef RES_C
 
 #ifndef lint
-static  char rcsid[] = "@(#)$Id: res.c,v 1.21.2.12 2003/10/11 01:13:04 chopin Exp $";
+static  char rcsid[] = "@(#)$Id: res.c,v 1.21.2.13 2003/10/11 13:25:14 chopin Exp $";
 #endif
 
 /* #undef	DEBUG	/* because there is a lot of debug code in here :-) */
@@ -479,7 +479,7 @@ Reg	ResRQ	*rptr;
 	    }
 	else
 	    {
-		(void)sprintf(ipbuf, "%x.%x.%x.%x.%x.%x.%x.%x.%x.%x.%x.%x.%x.%x.%x.%x.%x.%x.%x.%x.%x.%x.%x.%x.%x.%x.%x.%x.%x.%x.%x.%x.ip6.int.",
+		(void)sprintf(ipbuf, "%x.%x.%x.%x.%x.%x.%x.%x.%x.%x.%x.%x.%x.%x.%x.%x.%x.%x.%x.%x.%x.%x.%x.%x.%x.%x.%x.%x.%x.%x.%x.%x.ip6.%s.",
 		(u_int)(cp[15]&0xf), (u_int)(cp[15]>>4),
 		(u_int)(cp[14]&0xf), (u_int)(cp[14]>>4),
 		(u_int)(cp[13]&0xf), (u_int)(cp[13]>>4),
@@ -495,7 +495,11 @@ Reg	ResRQ	*rptr;
 		(u_int)(cp[3]&0xf), (u_int)(cp[3]>>4),
 		(u_int)(cp[2]&0xf), (u_int)(cp[2]>>4),
 		(u_int)(cp[1]&0xf), (u_int)(cp[1]>>4),
-		(u_int)(cp[0]&0xf), (u_int)(cp[0]>>4));
+		(u_int)(cp[0]&0xf), (u_int)(cp[0]>>4),
+#ifdef SIXBONE_HACK
+		(cp[0] == 0x3f && cp[1] == 0xfe) ? "int" :
+#endif
+		"arpa");
 	    }
 #else
 	cp = (u_char *)&numb->s_addr;
